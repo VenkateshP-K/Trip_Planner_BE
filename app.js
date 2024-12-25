29928e26ae25fa9e6c7e33589e240526c55a71e1
Venkatesh <venkateshpk06@gmail.com>
2024-12-25 17:04:25 +0530

    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['https://library-management-pk.netlify.app','http://localhost:5173', 'https://library-management-system-be.onrender.com'],
+  origin: ['http://localhost:5173', 'https://trip-planner-be.onrender.com/api'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],

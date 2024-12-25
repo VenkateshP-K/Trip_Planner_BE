2f46da47c7427bc3b354d3f4257fbfd184682d22
Venkatesh <venkateshpk06@gmail.com>
2024-12-25 17:06:09 +0530

    commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://trip-planner-be.onrender.com/api'],
+  origin: ['http://localhost:5173', 'https://trip-planner-be.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
